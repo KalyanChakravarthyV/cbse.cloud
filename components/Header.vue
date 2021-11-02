fa3a66a534aabbb08e5b407d9be5b50bf0d26822
Kalyan Chakravarthy V <KalyanChakravarthyV@gmail.com>
2021-11-03 00:29:05 +0530

    Draft version with carbon

---
+++ components/Header.vue
@@ -0,0 +1,74 @@
+<template>
+  <!--    
+<header class="header">
+        <ul class="nav-items">
+            <li>
+                <nuxt-link to="/">Home</nuxt-link>
+            </li>
+            <li>      <nuxt-link to="/about">About</nuxt-link>
+
+            </li>
+            <li>      <nuxt-link to="/contact">Contact</nuxt-link>
+
+            </li>
+        </ul>
+    </header>
+    -->
+
+  <cv-overflow-menu :label="label">
+    <cv-overflow-menu-item primary-focus>
+      <nuxt-link to="/">Home</nuxt-link>
+    </cv-overflow-menu-item>
+    <cv-overflow-menu-item>
+      <nuxt-link to="/about">About</nuxt-link>
+    </cv-overflow-menu-item>
+    <cv-overflow-menu-item itemref="/contact">
+      <nuxt-link to="/contact">Contact</nuxt-link>
+    </cv-overflow-menu-item>
+  </cv-overflow-menu>
+</template>
+
+<style scoped>
+/*
+.header {
+    width: 100%;
+    height: 60px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgb(211, 112, 236);
+}
+
+.nav-items {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    width: 80%;
+}
+
+.nav-items a {
+    text-decoration: none;
+    color: black;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+.nav-items a:hover,
+.nav-items a:active {
+    color: white;
+}
+
+*/
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      label: "Home",
+    };
+  },
+};
+</script>

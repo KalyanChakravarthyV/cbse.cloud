74b8bfc39be5c5563c4bc1efa55e5281a042f9df
Kalyan Chakravarthy V <KalyanChakravarthyV@gmail.com>
2023-01-01 17:34:39 +0530

    Beta-Release 01/01

--- components/SearchPanel.vue
+++ components/SearchPanel.vue
@@ -6,49 +6,130 @@
       v-model="sQ"
       placeholder="for e.g: Trignometry"
     />
-    <cv-button @click="onClick" >Go!</cv-button>
-    <cv-modal :visible="visible" @modal-hidden="modalClosed">
+    <cv-button @click="onClick">Go!</cv-button>
+    <!-- <cv-modal :visible="visible" @modal-hidden="modalClosed">
       <template slot="title">Welcome to cbse.cloud</template>
       <template slot="content">
         <p>
-          Our site is not ready yet{{sQ ? ' but stay tuned to know more about \''+sQ+'\'' : '!'}} .
+          Our site is not ready yet{{
+            sQ ? " but stay tuned to know more about '" + sQ + "'" : "!"
+          }}
+          .
         </p>
       </template>
-    </cv-modal>
+    </cv-modal> -->
+
+    <SearchResults :searchResults="searchResults"/>
+
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'SearchPanel',
-    data() {
-      return {
-        sQ: '',
-        visible: false,
-      };
+export default {
+  name: "SearchPanel",
+  data() {
+    return {
+      sQ: "",
+      visible: false,
+      searchResults : []
+          
+    };
+  },
+  methods: {
+    onClick() {
+      this.visible = true;
+      this.search();
     },
-    methods: {
-      onClick() {
-        this.visible = true;
-      },
-      modalClosed() {
-        this.visible = false;
-      },
+    modalClosed() {
+      this.visible = false;
     },
-  };
+
+    getElasticSearchQuery(qString){
+
+      let query = {
+          query: {
+            match: {
+              data: {
+                query: qString,
+              },
+            },
+          },
+
+          highlight: {
+            pre_tags: ["<bold><em>"],
+            post_tags: ["</em></bold>"],
+            fields: {
+              data: {},
+            },
+          },
+
+          fields: [
+            "classNum",
+            "class",
+            "pageNumber",
+            "subjectName",
+            "chapter",
+            "pdfUrl",
+          ],
+          _source: false, 
+        }
+
+      return JSON.stringify(query)
+
+    },
+    search() {
+      this.responseAvailable = false
+
+      this.searchResults = []
+      
+
+      fetch("https://elastic.cbse.cloud/ncert/_search", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: this.getElasticSearchQuery(this.sQ),
+      })
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          } else {
+            alert(
+              "Server returned " + response.status + " : " + response.statusText
+            );
+          }
+        })
+        .then((response) => {
+
+          console.log(JSON.stringify(response));
+
+          response.hits.hits.forEach(element => {
+            this.searchResults.push([element.fields.class[0], element.fields.subjectName[0],element.fields.chapter[0] ,element.highlight.data.join(),element.fields.pdfUrl[0]+"#page="+element.fields.pageNumber[0] ])
+            console.log([element.fields.class[0], element.fields.subjectName[0],element.fields.chapter[0], element.fields.pageNumber[0],element.fields.pdfUrl[0]])
+          });
+
+          // this.result = response.body;
+          // this.responseAvailable = true;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
 </script>
 
 <style>
-  .sample {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    max-width: 600px;
-    margin: 5% auto;
-  }
+.sample {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 600px;
+  margin: 5% auto;
+}
 
-  .cv-text-input {
-    margin: 30px 0;
-  }
+.cv-text-input {
+  margin: 30px 0;
+}
 </style>
---
+++ components/SearchResults.vue
@@ -0,0 +1,74 @@
+<template>
+  <!-- width: 300px;-->
+
+  <div style="margin: 6px; padding: 10px">
+    <!-- <cv-data-table
+      :title="title"
+      :columns="columns"
+      :data="searchResults"
+      :pagination="basicPagination"
+      :overflow-menu="true"
+      ref="table"
+      :hidden="visible"
+      use_htmlData="true"
+      >{{ msg }}</cv-data-table
+    > -->
+    
+
+    <cv-data-table
+      :columns="columns"
+      :pagination="basicPagination"
+      ref="table"
+    >
+      <template v-if="use_htmlData" slot="data">
+        <cv-data-table-row
+          v-for="(row, rowIndex) in searchResults"
+          :key="`${rowIndex}`"
+          :value="`${row}`"
+        >
+
+        <cv-data-table-cell v-for="(cell, cellIndex) in row" :key="`${cellIndex}`" :value="`${cellIndex}`" v-html="cell"></cv-data-table-cell>
+
+        
+        </cv-data-table-row>
+
+      </template>
+    </cv-data-table>
+    
+  </div>
+    
+</template>
+  
+  <script>
+export default {
+  name: "SearchResults",
+  props: {
+    msg: String,
+    searchResults: Array,
+  },
+
+  data() {
+    return {
+      title: "Search Results",
+      visible: false,
+      use_htmlData: true,
+      basicPagination: false,
+      columns: [
+        "Class/Grade",
+        "Subject",
+        "Chapter",
+        "Highlights",
+        "URL",
+      ],
+    };
+  },
+  methods: {
+    actionSelected(data) {
+      console.log(data);
+    },
+
+    onSubmit() {},
+  },
+};
+</script>
+  
